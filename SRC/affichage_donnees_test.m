6a8f98c19b9f36d17e817822a0a55ecd12ea42b0
jonathan5199 <114910017+jonathan5199@users.noreply.github.com>
2022-12-06 02:16:20 +0100

    Update affichage_donnees_test.m

--- SRC/affichage_donnees_test.m
+++ SRC/affichage_donnees_test.m
@@ -11,9 +11,9 @@ participant=["pere" "frere" "mere"]; %vecteur contenant la composante variable d
 particip_number=1; %choix du dataset
 
 
-%% chargement des données 
+%% chargement des données
 
-load(['..\DAT\shorten_data_' char(participant(particip_number)) '.mat']) ; 
+load(['..\DAT\shorten_data_' char(participant(particip_number)) '.mat']) ;
 N=size(data,1);
 
 time=data(:,1); % y soustraire data(1,1) dans le cadre d'un fichier unix epoch
@@ -21,7 +21,7 @@ x=data(:,2);
 y=data(:,3);
 z=data(:,4);
 
-%% Centrage des données 
+%% Centrage des données
 data_c(N,3)=0;
 
 for i=1:3
@@ -46,11 +46,11 @@ clear B A
 
 filtered_enmo(1:N)=0; %initialisation de la matrice enmo pour éviter son changement de taille à chaque itération
 for i=1:N
-vector=[xfilt(i) yfilt(i) zfilt(i)];
-filtered_enmo(i)=norm(vector)-1; %euclidean norm minus one
-if filtered_enmo(i)<0
-   filtered_enmo(i)=0; %valeurs négatives ramenées à 0 
-end
+    vector=[xfilt(i) yfilt(i) zfilt(i)];
+    filtered_enmo(i)=norm(vector)-1; %euclidean norm minus one
+    if filtered_enmo(i)<0
+        filtered_enmo(i)=0; %valeurs négatives ramenées à 0
+    end
 end
 
 clear vector i
@@ -61,13 +61,40 @@ window=1:60*Fs:N; % donne l'index de time de chaque début de plage de 60 sec
 moyepoch(1:size(window,2)-1)=0;
 
 for i = 1:size(window,2)-1
-moyepoch(1,i)=mean(filtered_enmo(window(i):window(i)+(60*Fs)-1));
+    moyepoch(1,i)=mean(filtered_enmo(window(i):window(i)+(60*Fs)-1));
 end
 
+%% cut-points
+
+cuts=[0.030 0.090 0.250];
+points(size(moyepoch,2)+1,1:4)=0;
+
+for i=1:size(moyepoch,2)
+    if moyepoch(1,i)<cuts(1)
+        points(i,1)=1;
+        points(end,1)=points(end,1)+1;
+    elseif moyepoch(1,i)<cuts(2)
+        points(i,2)=1;
+        points(end,2)=points(end,2)+1;
+    elseif moyepoch(1,i)<cuts(3)
+        points(i,3)=1;
+        points(end,3)=points(end,3)+1;
+    else
+        points(i,4)=1;
+        points(end,4)=points(end,4)+1;
+    end
+end
+
+%% Proportions de mouvement par intensité d'activité
+
+percentact(1,:)= ["SED" "LA" "MA" "HA"];
+for i = 1:4
+   percentact(2,i)=round((points(end,i)/size(moyepoch,2))*100,2);
+end
 
 %%  Affichage
 
-%Données brutes 
+%Données brutes
 
 figure("WindowState",'maximized')
 t=tiledlayout(3,1);
@@ -119,7 +146,7 @@ ylabel('accélération (g)')
 
 xlabel(t,'temps (s)')
 
-%% Données filtrées 
+%% Données filtrées
 
 figure("WindowState",'maximized')
 t=tiledlayout(4,1);
@@ -161,4 +188,20 @@ xlim([time(1,1) time(end,1)])
 ylabel('accélération moyenne (g)')
 xlabel('temps (s)')
 
+% v(:,1)=time(Fs*60*find(points(:,2)==1)+30);
+% v(:,2)=moyepoch(find(points(:,2)==1));
+% hold on
+% plot(v(:,1),v(:,2),'*')
+% % plot(time(window(1:end-1))+30*Fs, moyepoch(find(points(:,3)==1)) ,'*','g')
+% % plot(time(window(1:end-1))+30*Fs, moyepoch(find(points(:,4)==1)) ,'*','r')
+% hold off
 
+Color_window={'white','#EDB120','#D95319','#FF0000'};
+patchingcuts=[0 0.030 0.090 0.250 0.4];
+hold on
+for i=1:4
+zone=[0 patchingcuts(i); time(end,1) patchingcuts(i); time(end,1) patchingcuts(i+1); 0 patchingcuts(i+1)]; %x puis y
+f=[1 2 3 4];
+patch('Faces',f,'Vertices',zone,'FaceColor',Color_window{i},'FaceAlpha',.3)
+end
+hold off
